5458b0d4a9396d95f994d5bc19682590da77433a
Giovanni Domenico Lombardi <SpookyButter@gmail.com>
2014-04-26 17:46:31 +0200

    CAFEBABE
    DEADBEEF

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeManager : MonoBehaviour {
+
+    public float dayHours;
+    private float curTime;
+
+	// Use this for initialization
+	void Start () {
+        curTime = dayHours;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        curTime -= Time.deltaTime;
+        print(curTime);
+        if(curTime < 0)
+        {
+            
+        }
+	}
+}

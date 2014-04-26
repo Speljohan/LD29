6338d63c789bca34fcd4b09833a52bf4f367532f
Johan Ljungberg <johan.f.ljungberg@gmail.com>
2014-04-26 15:26:58 +0200

    meny fickses

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AI : MonoBehaviour {
+
+    private CharacterController controller;
+    private Transform transform;
+
+    public float speed = 2.0f;
+
+    public List<GameObject> nodes = new List<GameObject>();
+    private int currentIndex;
+    private bool finished = false;
+
+    private Vector3 lastPosition;
+
+	// Use this for initialization
+	void Start () {
+        this.controller = GetComponent<CharacterController>();
+        controller.radius = 0;
+        controller.height = 0;
+        this.transform = GetComponent<Transform>();
+        transform.position = new Vector3(-4f, 0.2f, 1);
+        this.currentIndex = 0;
+        //nodes = new List<GameObject>();
+    }
+
+    public void Next()
+    {
+        currentIndex++;
+    }
+
+    public void Update()
+    {
+        if (currentIndex >= nodes.Count)
+        {
+            return;
+        }
+
+        GameObject node = nodes[currentIndex];
+
+        Transform targetPosition = node.GetComponent<Transform>();
+
+        if ((transform.position - targetPosition.position).magnitude < 0.1f)
+        {
+            Next();
+        }
+        else
+        {
+            if (transform.position == lastPosition)
+            {
+                Vector3 direction = (targetPosition.position - transform.position).normalized;
+
+                controller.Move(new Vector3(direction.x * Time.deltaTime * speed, direction.y * Time.deltaTime * speed, 0));
+            }
+            lastPosition = transform.position;
+        }
+    }
+}
---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class Main : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        CreateRandomNpc();
+	}
+
+    void CreateRandomNpc()
+    {
+        GameObject obj = new GameObject();
+        obj.tag = "NPC";
+        SpriteRenderer r = obj.AddComponent<SpriteRenderer>();
+        r.sprite = Resources.Load<Sprite>("Sprites/Character_0");
+        r.sortingOrder = 1;
+        CharacterController cc = obj.AddComponent<CharacterController>();
+        AI ai = obj.AddComponent<AI>();
+        ai.nodes.AddRange(GameObject.FindGameObjectsWithTag("MovementNode"));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}

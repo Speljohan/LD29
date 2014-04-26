262041db51898ec88238930667ee982c75810a77
Johan Ljungberg <johan.f.ljungberg@gmail.com>
2014-04-26 18:28:29 +0200

    HA

---
+++ Assets/Scripts/NodeRule.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NodeRule : MonoBehaviour {
+
+    public bool west, east, north, south;
+
+    public int GetRandomTraversibleNode()
+    {
+        if (north) return 0;
+        if (west) return 1;
+        if (east) return 2;
+        if (south) return 3;
+        return 0;
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/TriggerCollision.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void OnTriggerEnter(Collider target)
+    {
+        target.gameObject.BroadcastMessage("NewTarget");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

a12f16f5f1987336abea9d1281d2fdf78c17d137
Johan Ljungberg <johan.f.ljungberg@gmail.com>
2014-04-26 10:21:41 +0200

    HAHA

---
+++ Assets/Scripts/PlayerInputTest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInputTest : MonoBehaviour {
+
+	float moveSpeed = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+		float y = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+		GetComponent<CharacterController>().Move(new Vector3(x, y, 0));
+	}
+}

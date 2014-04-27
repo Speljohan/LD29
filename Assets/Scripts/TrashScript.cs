1d6628a4ba7b95b26146c458920ee166baba5311
Giovanni Domenico Lombardi <SpookyButter@gmail.com>
2014-04-27 16:03:40 +0200

    Eyyy
    Fwofq

---
+++ Assets/Scripts/TrashScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrashScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.E))
+        {
+            //Do dialogue "Hey I found some food";
+            GameObject go = GameObject.FindGameObjectWithTag("Player");
+            go.GetComponent<PlayerScript>().food++;
+
+        }
+	}
+}

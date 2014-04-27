c6b141b6fb1fcca3e4b85f0854fa8e61a34c19ab
Giovanni Domenico Lombardi <SpookyButter@gmail.com>
2014-04-27 16:32:18 +0200

    PlayerMove
    HambaBumbpa

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -2,15 +2,31 @@
 using System.Collections;
 
 public class PlayerMove : MonoBehaviour {
-
+    private Vector2 mover;
+    
+    private Rigidbody2D contr;
+    public float moveSpeed = 4;
 	// Use this for initialization
 	void Start () {
-	
+        contr = GetComponent<Rigidbody2D>();
+        contr.gravityScale = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         //float x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         //float y = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        // 4 - Movement per direction
+        mover = new Vector2(
+          moveSpeed * inputX,
+          moveSpeed * inputY);
+        
 	}
+    void FixedUpdate()
+    {
+        rigidbody2D.velocity = mover;
+    }
 }

24401f0c207b2ab8834a6c8be4494892cd7196ce
Giovanni Domenico Lombardi <SpookyButter@gmail.com>
2014-04-27 16:21:48 +0200

    PUUUSH
    JIGEJIGE

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //float x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        //float y = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+	}
+}

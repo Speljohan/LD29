eb054324e0bff055523c57c666609906437cad69
Giovanni Domenico Lombardi <SpookyButter@gmail.com>
2014-04-27 12:34:51 +0200

    Nodes yo
    Oh mah gawd

---
+++ Assets/Scripts/AITest.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AITest : MonoBehaviour {
+
+    private Rigidbody2D controller;
+    public Vector2 speed = new Vector2(1,1);
+    public Vector2 moveTo = new Vector2(0, 0);
+	// Use this for initialization
+    private Vector2 moveDirection = new Vector2(1, 0);
+    private Vector2 nextMoveDirection = new Vector2(0, 0);
+	void Start () {
+        this.controller = GetComponent<Rigidbody2D>();
+        controller.gravityScale = 0;
+        moveDirection = new Vector2(1, 0);
+    
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+       // print("OIL");
+        Vector2 goPos = gameObject.GetComponent<BoxCollider2D>().transform.position;
+        if (controller.velocity.x == 1 && controller.velocity.y == 0)
+        {
+            if (goPos.x >= moveTo.x)
+            {
+                moveDirection = nextMoveDirection;
+                print("IM MOVING YO");
+            }
+        }
+        if (controller.velocity.x == -1 && controller.velocity.y == 0)
+        {
+            if (goPos.x <= moveTo.x)
+            {
+                moveDirection = nextMoveDirection;
+            }
+        }
+        if (controller.velocity.x == 0 && controller.velocity.y == 1)
+        {
+            if (goPos.y >= moveTo.y)
+            {
+                moveDirection = nextMoveDirection;
+            }
+        }
+        if (controller.velocity.x == 0 && controller.velocity.y == -1)
+        {
+            if (goPos.y <= moveTo.y)
+            {
+                moveDirection = nextMoveDirection;
+            }
+        }
+        
+        
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        print("WE HIT OIL!");
+
+        NodeRule node = otherCollider.gameObject.GetComponent<NodeRule>();
+
+        Vector2 nodePos = node.GetComponent<BoxCollider2D>().size;
+        Vector2 goHBp = gameObject.GetComponent<BoxCollider2D>().transform.position;
+        if (controller.velocity.x == 1 && controller.velocity.y == 0)
+        {
+            moveTo = new Vector2(goHBp.x + nodePos.x, goHBp.y);
+        }
+                 if (controller.velocity.x == -1 && controller.velocity.y == 0)
+        {
+            moveTo = new Vector2(goHBp.x - nodePos.x, goHBp.y);
+        }
+                 if (controller.velocity.x == 0 && controller.velocity.y == 1)
+        {
+            moveTo = new Vector2(goHBp.x, goHBp.y + nodePos.y);
+        }
+                 if (controller.velocity.x == 0 && controller.velocity.y == -1)
+        {
+            moveTo = new Vector2(goHBp.x, goHBp.y - nodePos.y);
+        }
+
+       // gameObject.transform.position = node.GetComponent<BoxCollider2D>().transform.position;
+        nextMoveDirection = node.GetRandomTraversibleVector(controller.velocity);
+        
+    }
+
+    void FixedUpdate()
+    {
+        controller.velocity = moveDirection;
+    }
+
+}
--- Assets/Scripts/NodeRule.cs
+++ Assets/Scripts/NodeRule.cs
@@ -7,19 +7,65 @@ public class NodeRule : MonoBehaviour {
 
     public int GetRandomTraversibleNode()
     {
-        if (north) return 0;
-        if (west) return 1;
-        if (east) return 2;
-        if (south) return 3;
-        return 0;
+        /*if (north) return new Vector2(0,1);
+        if (west) return new Vector2(-1, 0);
+        if (east) return new Vector2(1, 0);
+        if (south) return new Vector2(0, -1);*/
+        return 1;
+    }
+    public Vector2 GetRandomTraversibleVector(Vector2 vec)
+    {
+        Random.seed = (int)System.DateTime.Now.Ticks;
+
+        int aL = 0;
+        int iP = 0;
+
+        if (north) aL++;
+        if (west) aL++;
+        if (east) aL++;
+        if (south) aL++;
+
+        Vector2[] vectors = new Vector2[aL];
+
+        if (north)
+        {
+           vectors[iP] = new Vector2(0, 1);
+           iP++;
+        }
+        if (west)
+        {
+            vectors[iP] = new Vector2(-1, 0);
+            iP++;
+        }
+
+        if (east)
+        {
+            vectors[iP] =  new Vector2(1, 0);
+            iP++;
+        }
+        if (south)
+        {
+            vectors[iP] = new Vector2(0, -1);
+            iP++;
+        }
+
+
+        int iiiii = Random.Range(0, aL);
+        print("NODE RANDOM FAGGOT " + iiiii);
+        return vectors[iiiii];
     }
 	// Use this for initialization
 	void Start () {
-	
+       
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        print("WE HIT gold!");
+    }
 }
